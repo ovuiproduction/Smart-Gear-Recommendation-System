791bb81b056144a65e3b6786c4c72455879fea38
ONKAR ANIL WAGHMODE <136163415+ovuiproduction@users.noreply.github.com>
2024-04-14 10:47:46 +0530

    Update MyForegroundService.java

--- app/src/main/java/com/example/smart_gear_recommandation_system/MyForegroundService.java
+++ app/src/main/java/com/example/smart_gear_recommandation_system/MyForegroundService.java
@@ -149,7 +149,7 @@ public class MyForegroundService extends Service {
     }
 
     private void fetchDataFromAPI(final DataCallback callback) {
-        String uri = "https://api.thingspeak.com/channels/2473881/feeds.json?api_key=<Your_READ_API_KEY>&results=1";
+        String uri = "https://api.thingspeak.com/channels/<YOUR_CHANNEL_ID>/feeds.json?api_key=<Your_READ_API_KEY>&results=1";
         StringRequest stringRequest = new StringRequest(Request.Method.GET, uri, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
